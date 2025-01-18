6f79d0ea68596537b6b27cf84fc2fd3ef110a16e
vosmiic <44092756+vosmiic@users.noreply.github.com>
2025-01-18 14:45:52 +0000

    added ability to create and get room from API

--- VideoStreamBackend/Controllers/RoomController.cs
+++ VideoStreamBackend/Controllers/RoomController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using VideoStreamBackend.Identity;
+using VideoStreamBackend.Models;
 
 namespace VideoStreamBackend.Controllers;
 
@@ -8,14 +11,31 @@ namespace VideoStreamBackend.Controllers;
 [Route("[controller]")]
 public class RoomController : Controller {
     private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public RoomController(ApplicationDbContext context) {
+    public RoomController(ApplicationDbContext context, UserManager<ApplicationUser> userManager) {
         _context = context;
+        _userManager = userManager;
     }
 
     [HttpPost]
     [Authorize]
-    public string CreateRoom([FromBody] string roomName) {
-        return roomName;
+    public async Task<ActionResult> CreateRoom([FromBody] string roomName) {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        if (user == null) return Unauthorized();
+        Room room = new Room {
+            Name = roomName,
+            Owner = user
+        };
+        _context.Rooms.Add(room);
+        await _context.SaveChangesAsync();
+        return Ok(room.Id);
+    }
+    
+    [HttpGet]
+    [Route("{roomId}")]
+    public async Task<ActionResult> GetRoom([FromRoute] Guid roomId) {
+        var room = await _context.Rooms.FirstOrDefaultAsync(room => room.Id == roomId);
+        return room != null ? Ok(room) : NotFound();
     }
 }
