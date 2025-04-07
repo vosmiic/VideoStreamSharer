187c90b35d9237d7e3bb6519a91a944ede730651
vosmiic <44092756+vosmiic@users.noreply.github.com>
2025-04-07 21:13:57 +0100

    change room config

--- VideoStreamBackend/Models/Configuration/RoomConfiguration.cs
+++ VideoStreamBackend/Models/Configuration/RoomConfiguration.cs
@@ -6,6 +6,11 @@ namespace VideoStreamBackend.Models.Configuration;
 public class RoomConfiguration : IEntityTypeConfiguration<Room> {
     public void Configure(EntityTypeBuilder<Room> builder) {
         builder.HasOne<ApplicationUser>(r => r.Owner).WithMany();
-        builder.HasMany(r => r.Queue).WithOne(q => q.Room);
+        builder
+            .HasMany(r => r.Queue)
+            .WithOne(q => q.Room)
+            .IsRequired()
+            .HasForeignKey(item => item.RoomId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
--- VideoStreamBackend/Models/QueueItem.cs
+++ VideoStreamBackend/Models/QueueItem.cs
@@ -4,7 +4,9 @@ namespace VideoStreamBackend.Models;
 
 public class QueueItem : GuidPrimaryKey {
     [JsonIgnore]
-    public Room Room { get; init; }
+    public virtual Room Room { get; init; }
+    public Guid RoomId { get; set; }
     public string Title { get; set; }
     public string? ThumbnailLocation { get; set; }
+    public int Order { get; set; }
 }
