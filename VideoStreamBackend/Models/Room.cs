941342f842146c68d402ab74abaf22685f5a5bba
vosmiic <44092756+vosmiic@users.noreply.github.com>
2025-06-02 19:45:52 +0100

    adding status to room

--- VideoStreamBackend/Models/Room.cs
+++ VideoStreamBackend/Models/Room.cs
@@ -4,4 +4,10 @@ public class Room : GuidPrimaryKey {
     public virtual ApplicationUser Owner { get; set; }
     public string Name { get; set; }
     public virtual ICollection<QueueItem> Queue { get; set; } = new List<QueueItem>();
+    public Status Status { get; set; }
+}
+
+public enum Status {
+    Playing = 1,
+    Paused = 2
 }
