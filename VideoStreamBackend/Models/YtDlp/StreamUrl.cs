18c071b00b1921951e59c775fa4b478811d223bf
vosmiic <44092756+vosmiic@users.noreply.github.com>
2025-07-28 19:17:15 +0100

    added expiry to stream url

--- VideoStreamBackend/Models/YtDlp/StreamUrl.cs
+++ VideoStreamBackend/Models/YtDlp/StreamUrl.cs
@@ -3,6 +3,10 @@ namespace VideoStreamBackend.Models.YtDlp;
 public class StreamUrl {
     public string Url { get; set; }
     public StreamType StreamType { get; set; }
+    /// <summary>
+    /// Expiry in unix time seconds.
+    /// </summary>
+    public long Expiry { get; set; }
 }
 
 public enum StreamType {
